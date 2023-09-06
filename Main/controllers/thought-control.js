3c8a2a566aef80af65c481f69e6af533905c96f4
Marinah1031 <marinahtam@gmail.com>
2023-09-06 01:27:46 -0700

    delete Thought

--- Main/controllers/thought-control.js
+++ Main/controllers/thought-control.js
@@ -3,7 +3,7 @@ const { Thought, User } = require('../models');
 const handleError = (res, err) => {
     console.error(err);
     res.status(500).json(err);
-}; 
+};
 
 const thoughtController = {
     //getting all thoughts
@@ -12,51 +12,100 @@ const thoughtController = {
             const dbThoughtData = await Thought.find().sort({ createdAt: -1 });
             res.json(dbThoughtData);
         } catch (err) {
-          handleError(res, err);
+            handleError(res, err);
         }
     },
 
     async getOneThought(req, res) {
         try {
             const dbThoughtData = await Thought.findById(req.params.thoughtId);
-                     if (!dbThoughtData){
-                        return res.status(404).json ({ 
-                            message: 'No thought associated with this ID' 
-                        });
-                    }
+            if (!dbThoughtData) {
+                return res.status(404).json({
+                    message: 'No thought associated with this ID'
+                });
+            }
 
-                    res.json(dbThoughtData);
-                    }catch (err) {
-                        handleError(res, err);
-                    }
-                },
-     async createThought(req, res) {
+            res.json(dbThoughtData);
+        } catch (err) {
+            handleError(res, err);
+        }
+    },
+    async createThought(req, res) {
         try {
             const dbThoughtData = await Thought.create(req.body);
 
             const dbUserData = await User.findByIdAndUpdate(
                 req.body.userId,
-                { $push: {
-                    thoughts: dbThoughtData._id
-                }},
+                {
+                    $push: {
+                        thoughts: dbThoughtData._id
+                    }
+                },
                 { new: true }
             );
 
             if (!dbUserData) {
-                return res.status(404).json({ 
+                return res.status(404).json({
                     message: 'Thought created with no user associated with this ID'
                 });
             }
 
-            res.json({ 
+            res.json({
                 message: 'Thoughts created successfully'
             });
         } catch (err) {
             handleError(res, err);
         }
-     },
+    },
 
-     
+    async updateThought(req, res) {
+        try {
+            const dbThoughtData = await Thought.findByIdAndUpdate(
+                req.params.thoughtId,
+                req.body,
+                {
+                    new: true,
+                    runValidators: true
+                }
+            );
+
+            if (!dbThoughtData) {
+                return res.status(404).json({
+                    message: 'No thought associated with this ID'
+                });
+
+                res.json(dbThoughtData);
+            } catch (err) {
+                handleError(res, err);
+            }
+        },
+
+        async deleteThought(req, res) {
+            try {
+                const dbThoughtData = await Thought.findByIdAndRemove(req.params.thoughtId);
+
+                if (!dbThoughtData) {
+                    return res.status(404).json({
+                        message: 'No thought associated with this ID'
+                    });
+                }
+
+                const dbUserData = await User.findByIdAndUpdate(
+                    dbThoughtData.userId,
+                    {
+                        $pull:
+                        {
+                            thoughts:
+                                dbThoughtData._id
+                        }
+                    },
+                    { new: true }
+                );
+
+                
+            }
+        }
+    }
 };
 
 module.exports = thoughtController;
