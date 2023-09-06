a66db03f21e33ae22a363c856455b0c19a1b0bfd
Marinah1031 <marinahtam@gmail.com>
2023-09-06 01:13:26 -0700

    thought controls for getting one thought

--- Main/controllers/thought-control.js
+++ Main/controllers/thought-control.js
@@ -1,15 +1,36 @@
 const { Thought, User } = require('../models');
 
+const handleError = (res, err) => {
+    console.error(err);
+    res.status(500).json(err);
+}; 
+
 const thoughtController = {
+    //getting all thoughts
     async getThoughts(req, res) {
         try {
             const dbThoughtData = await Thought.find().sort({ createdAt: -1 });
             res.json(dbThoughtData);
         } catch (err) {
-            console.error(err);
-            res.status(500).json(err);
+          handleError(res, err);
         }
     },
+
+    async getOneThought(req, res) {
+        try {
+            const dbThoughtData = await Thought.findById(req.params.thoughtId);
+                     if (!dbThoughtData){
+                        return res.status(404).json ({ 
+                            message: 'No thought associated with this ID' 
+                        });
+                    }
+
+                    res.json(dbThoughtData);
+                    }catch (err) {
+                        handleError(res, err);
+                    }
+                },
+     
 };
 
 module.exports = thoughtController;
