01591c4fd5ed68e7b2276f401f9527041ae66d6e
Marinah1031 <marinahtam@gmail.com>
2023-09-05 17:31:59 -0700

    adding express.json and routes with the app.use function

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const routes = require('/routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`the API server is running on the port ${PORT}`);
+    });
+});
