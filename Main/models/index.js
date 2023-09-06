800d426d3044fcbaaa7d2b6ca36c9a3162c23647
Marinah1031 <marinahtam@gmail.com>
2023-09-05 23:55:02 -0700

    api routes fixing and addding pathways in models

--- Main/models/index.js
+++ Main/models/index.js
@@ -1,8 +1,4 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes.js');
-const thoughtRoutes = require('./thought-routes');
+const User = require('./User');
+const Thought = require('./Thought');
 
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
-
-module.exports = router;
+module.exports = {User, Thought };
