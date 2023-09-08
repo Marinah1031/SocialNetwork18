f91aa78ca6d740068d98f2f802b82758e0945bf2
Marinah1031 <marinahtam@gmail.com>
2023-09-07 18:35:17 -0700

    chaning port name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./Main/config/connection');
 const routes = require('./Main/routes');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(express.urlencoded({
